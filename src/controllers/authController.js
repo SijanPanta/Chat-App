4ef204c6b2fff5994658b4a7fd62ec1e582bc2e9
sijanpanta <cjan1102@gmail.com>
2026-02-01 14:43:23 +0545

    registeration api is completed at api/auth/register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import db from "../models/index.js";
+import user from "../models/user.js";
 
 const { User } = db;
 
@@ -7,13 +8,27 @@ export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    res
-      .status(201)
-      .json({
-        message: `name:${username},email:${email},password:${password}`,
-      });
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(409).json({ error: "User is already registered" });
+    }
 
-    // res.status(201).json({ message:  });
+    const hashedPassword=await bcrypt.hash(password,10);
+
+    const newUser=await User.create({
+      username,
+      email,
+      password_hash:hashedPassword
+    })
+
+     res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: newUser.id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
